9a68565390c9365a61cf48aceeeb0ed91e1e26e8
noorshiblee <noorshiblee@gmail.com>
2016-07-28 21:42:53 +0600

    Create main.swift

---
+++ main.swift
@@ -0,0 +1,35 @@
+
+import UIKit
+
+class main{
+
+  //run terminal command
+  func runTerminalCommand(launchPath:String, args: String...) -> Int32 {
+      let task = NSTask()
+      task.launchPath = launchPath
+      task.arguments = args       
+      task.currentDirectoryPath = self.readFromProjectInfoFile("xcodeprojectdir") //ex. User/username/Desktop/Xcodeproject/
+      task.launch()
+      task.waitUntilExit()
+      return task.terminationStatus
+    }//func ends here
+    
+  //get current time as string
+  func getCurrentTimeStamp() -> String{
+        
+      let date = NSDate()
+      let formatter = NSDateFormatter()
+      formatter.dateFormat = "yyyy-MM-dd-HH:mm:ss"
+      //formatter.timeZone = NSTimeZone(forSecondsFromGMT: 6)
+      formatter.locale = NSLocale(localeIdentifier: "en_US_POSIX")
+      return formatter.stringFromDate(date)
+      
+    }//func ends here
+}
+
+let run = main()
+
+var currentTime = run.getCurrentTimeStamp()
+run.runTerminalCommand("/bin/bash", args: "runXcodeBuild.sh","xcodeprojectLocation","schemeName","desiredLocationToSaveTestReort","\(currentTime)")
+
+
